ba47d375b17ad5736de6b5c1cde290170ce66d9d
pouria <pouria1778rezaee@gmail.com>
2026-08-29 14:38:42 +0330

    configure github pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  output: "export",
+
+  basePath: "/portfolio",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
